c9b915aa6ec778d237ac3c4098c1d5e204707ce0
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-19 04:39:37 +0000

    [ADD]: task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	rightNode->parent = parent;
 	rightNode->left = NULL;
 	rightNode->right = NULL;
-	if (parent->right == NULL){
+	if (parent->right == NULL)
+	{
 		parent->right = rightNode;
 	}
 	else

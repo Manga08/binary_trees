9246e1ade6e89d27df6e1f7a5effab3b173ab033
Manga08 <bijuu5445@gmail.com>
2020-07-20 22:56:43 -0500

    [Featured]: The task 9 and 11

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,22 @@
 #include "binary_trees.h"
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to traverse.
+ * Return: The height of the binary tree.
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-        binary_tree_t *new_node;
-        new_node = malloc(sizeof(binary_tree_t));
-        new_node->n = value;
-        new_node->parent = parent;
-        return new_node;
+	int count_left = 0, count_right = 0;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	if (tree->left)
+		count_left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		count_right = 1 + binary_tree_height(tree->right);
+
+	if (count_left > count_right)
+		return (count_left);
+	return (count_right);
 }

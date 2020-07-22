2a2045ed99347621d5ac7aafa8519e7b4f4c0415
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-22 03:00:00 +0000

    [UPDATE]: task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,27 +7,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *rightNode;
+	binary_tree_t *rightNode;
 
-        if (parent == NULL)
-                return (NULL);
+	if (parent == NULL)
+		return (NULL);
+	rightNode = malloc(sizeof(binary_tree_t));
+	if (rightNode == NULL)
+		return (NULL);
+	rightNode->n = value;
+	rightNode->parent = parent;
+	rightNode->left = NULL;
+	rightNode->right = NULL;
 
-        rightNode = malloc(sizeof(binary_tree_t));
-
-        if (rightNode == NULL)
-                return (NULL);
-
-        rightNode->n = value;
-        rightNode->parent = parent;
-        rightNode->left = NULL;
-        rightNode->right = NULL;
-
-        if (parent->right != NULL)
-        {
-                rightNode->right = parent->right;
-                parent->right->parent = rightNode;
-        }
-        parent->right = rightNode;
-        return (rightNode);
-
+	if (parent->right != NULL)
+	{
+		rightNode->right = parent->right;
+		parent->right->parent = rightNode;
+	}
+	parent->right = rightNode;
+	return (rightNode);
 }

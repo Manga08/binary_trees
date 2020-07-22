bf9825dd99c0b3ba28bb15a59c93471738506c1d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-22 03:42:39 +0000

    [ADD]: task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,23 +1,20 @@
 #include "binary_trees.h"
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * binary_tree_leaves - check how many leaves contain a node.
+ * @tree: Pointer to the node of tree
+ * Return: Return the count levels.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t cnt = 0;
 
-        if (tree == NULL)
-                return 0;
+	if (tree == NULL)
+		return (0);
 	if (tree->left != NULL)
 		cnt++;
 	if (tree->right != NULL)
 		cnt++;
 	if (tree->right == NULL && tree->left == NULL)
 		cnt++;
-        return cnt;
+	return (cnt);
 }

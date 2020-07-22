502ae311fd3bc3779550bbd9d78ee359990b8d22
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-22 03:49:59 +0000

    [update]: task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,11 +10,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
 	if (tree->left != NULL)
 		cnt++;
 	if (tree->right != NULL)
-		cnt++;
-	if (tree->right == NULL && tree->left == NULL)
 		cnt++;
 	return (cnt);
 }

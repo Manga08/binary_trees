d08b1c21b20a6bedefb5e408c4856e292e6c2efc
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-22 05:06:20 +0000

    [UPDATE]: task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -62,7 +62,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (binary_tree_balance(tree) == 0)
-		if (binary_tree_is_full == 1)
+		if (binary_tree_is_full(tree) == 1)
 			return (1);
 	return (0);
 }

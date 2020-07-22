f55b980c5f84461391cebfa6a7742a83807acd24
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-22 04:28:04 +0000

    [ADD]: task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,38 +1,35 @@
 #include "binary_trees.h"
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * score - Goes through a binary tree using pre-order.
+ * @node: Pointer to the node of tree
+ * Return: Return the count balance in right and left tree.
  */
 int score(binary_tree_t *node)
 {
 	int lr = 0;
 	int lf = 0;
-	printf("entre\n");
+
 	if (node == NULL)
 		return (0);
 	if (node->left == NULL && node->right == NULL)
 		return (1);
 	if (node->left)
 		lf = 1 + score(node->left);
-	else
-		lf = 0;
 	if (node->right)
 		lr = 1 + score(node->right);
-	else
-		lr = 0;
 	if (lf > lr)
 		return (lf);
 	else
 		return (lr);
 }
-
+/**
+ * binary_tree_balance - Function that return balance coefficient.
+ * @tree: Pointer to the node of tree
+ * Return: Return balance coefficient.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	printf("no pude entrar \n");
-	printf("muestrame esto\n");
-        return (score(tree->left) - score(tree->right));
+	if (tree == NULL)
+		return (0);
+	return (score(tree->left) - score(tree->right));
 }

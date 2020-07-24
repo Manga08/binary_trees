c1bd43c634174d14280f3d77a69e32663ff84aaf
Manga08 <bijuu5445@gmail.com>
2020-07-24 00:52:42 -0500

    [Fix]: The task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,11 +43,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+
 	if (tree->left != NULL && tree->right != NULL)
-		return (1 * binary_tree_is_full(tree->left) *
-			binary_tree_is_full(tree->right));
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+
 	return (0);
 }
 /**
